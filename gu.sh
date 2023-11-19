948b39a5aac99255efc2ddc085a5ab4700366ecb
BagiyaLakshmi <bagiyalakshmi59@gmail.com>
2023-11-19 22:36:21 +0530

    : Updates for 2023-11-19

--- gu.sh
+++ gu.sh
@@ -1,10 +1,20 @@
 git_push() {
 
-    echo "git push automatic"
+    echo "Git commiting with comments from txt file!! Hurrayyy!"
 
     git add . 
 
-    git commit -m "Be careful, for next commmit"
+    file_path = "/home/bagiya/tact/my-first-repo/comments.txt"
+    
+    no_of_line = `wc -l < $file_path`
+
+    echo $((no_of_line))
+
+    rando_number=`shuf -i 1-$((no_of_line)) -n 1`
+
+    line=`sed $((rando_number))!d $file_path`
+
+    git commit -m "$line: Updates for $(date +%F)"
 
     git push
 
