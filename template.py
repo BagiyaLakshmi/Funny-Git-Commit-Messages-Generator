7ebbe6a081cb992ff8d2832d44022e64c8b48c3c
BagiyaLakshmi <bagiyalakshmi59@gmail.com>
2023-11-19 21:48:37 +0530

    my first commit

---
+++ template.py
@@ -0,0 +1,16 @@
+'''
+Created on 
+
+@author:Bagiya Lakshmi S
+
+source:
+    https://stackoverflow.com/questions/370357/python-variable-scope-error
+'''
+
+def startpy():
+
+    print("Tact101")
+    
+
+if __name__ == '__main__':
+    startpy()
